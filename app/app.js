4fcb0ca7af9091983b504de67ace84a04a399494
Kevin Wong <kkwokwai22@gmail.com>
2017-08-02 01:13:14 -0700

    add modules

---
+++ app/app.js
@@ -0,0 +1,18 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.config(function() {
+
+    alert('kevin');
+
+});
+
+myNinjaApp.run(function() {
+
+    console.log('this is great');
+
+});
+
+myNinjaApp.controller(function() {
+
+
+});
